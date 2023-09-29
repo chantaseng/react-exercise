3e1cbe38fb9bdc2c18d41d5ae01612d02221447f
William Chan <chan.william.ts@gmail.com>
2023-09-29 12:27:01 -0400

    profile card v2, with condtional rendering &&

---
+++ 00-Challenges/01-Working-with-Components/02-profile-card-v2/src/index.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const skills = [
+  {
+    skill: 'HTML+CSS',
+    level: 'advanced',
+    color: '#2662EA',
+  },
+  {
+    skill: 'JavaScript',
+    level: 'advanced',
+    color: '#EFD81D',
+  },
+  {
+    skill: 'Web Design',
+    level: 'advanced',
+    color: '#C3DCAF',
+  },
+  {
+    skill: 'Git and GitHub',
+    level: 'intermediate',
+    color: '#E84F33',
+  },
+  {
+    skill: 'React',
+    level: 'advanced',
+    color: '#60DAFB',
+  },
+  {
+    skill: 'Svelte',
+    level: 'beginner',
+    color: '#FF3B00',
+  },
+];
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="speech.jpg" alt="Will" />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>William Chan</h1>
+      <p>
+        Front-end web developer who just graduated from Le Wagon Montreal. When
+        not coding, I like to go on dates with my wife and dog, Alto or browse
+        on ebay to grow my money collection.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <ul className="skill-list">
+      {skills.map(skill => (
+        <Skill
+          skill={skill.skill}
+          color={skill.color}
+          level={skill.level}
+          key={skill.skill}
+        />
+      ))}
+    </ul>
+  );
+}
+
+function Skill({ skill, color, level }) {
+  return (
+    <li className="skill" style={{ backgroundColor: color }}>
+      <span>{skill}</span>
+      <span>
+        {level === 'beginner' && '👶🏻'}
+        {level === 'intermediate' && '👍🏼'}
+        {level === 'advanced' && '💪🏼'}
+      </span>
+    </li>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
