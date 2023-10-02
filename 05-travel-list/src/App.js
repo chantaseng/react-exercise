d66897f4c388587154e9461e4b78704613105461
William Chan <chan.william.ts@gmail.com>
2023-10-01 22:17:20 -0400

    building a form and handling submission

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -1,7 +1,7 @@
 const initialItems = [
   { id: 1, description: 'Passports', quantity: 2, packed: false },
   { id: 2, description: 'Socks', quantity: 12, packed: true },
-  { id: 2, description: 'Charger', quantity: 1, packed: false },
+  { id: 3, description: 'Charger', quantity: 1, packed: false },
 ];
 
 export default function App() {
@@ -20,10 +20,27 @@ function Logo() {
 }
 
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e);
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-    </div>
+      <select>
+        {/* <option value={1}>1</option>
+        <option value={2}>2</option>
+        <option value={3}>3</option> */}
+        {Array.from({ length: 20 }, (_, i) => i + 1).map(num => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
   );
 }
 
@@ -32,7 +49,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map(item => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
