db1e8221596625c351de6965133adde6adb0d0ec
William Chan <chan.william.ts@gmail.com>
2023-10-06 18:38:00 -0400

    improving reusability in props

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
@@ -5,10 +6,26 @@ import ReactDOM from 'react-dom/client';
 
 import StarRating from './StarRating';
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>The movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={3} />
+    <Test />
   </React.StrictMode>
 );
