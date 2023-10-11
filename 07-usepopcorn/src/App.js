6f2bd3249942925f6a61f04a390672d6fd2482a7
William Chan <chan.william.ts@gmail.com>
2023-10-11 17:28:21 -0400

    cleaning up fetch

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -98,12 +98,16 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
+
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError('');
+
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
 
           if (!res.ok)
@@ -111,10 +115,15 @@ export default function App() {
 
           const data = await res.json();
           if (data.Response === 'False') throw new Error('Movie not found');
+
           setMovies(data.Search);
+          setError('');
         } catch (err) {
           console.error(err.message);
-          setError(err.message);
+
+          if (err.message !== 'AbortError') {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -126,6 +135,10 @@ export default function App() {
         }
       }
       fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
@@ -352,6 +365,10 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     function () {
       if (!title) return;
       document.title = `Movie | ${title}`;
+
+      return function () {
+        document.title = 'usePopcorn';
+      };
     },
     [title]
   );
