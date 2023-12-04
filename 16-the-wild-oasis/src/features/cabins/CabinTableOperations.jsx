57822a665fc291ef91bddb00022364217c13926c
William Chan <chan.william.ts@gmail.com>
2023-12-03 20:32:54 -0500

    sorting cabins

--- 16-the-wild-oasis/src/features/cabins/CabinTableOperations.jsx
+++ 16-the-wild-oasis/src/features/cabins/CabinTableOperations.jsx
@@ -1,5 +1,6 @@
 import TableOperations from '../../ui/TableOperations';
 import Filter from '../../ui/Filter';
+import SortBy from '../../ui/SortBy';
 
 function CabinTableOperations() {
   return (
@@ -10,6 +11,17 @@ function CabinTableOperations() {
           { value: 'all', label: 'All' },
           { value: 'no-discount', label: 'No discount' },
           { value: 'with-discount', label: 'With discount' },
+        ]}
+      />
+
+      <SortBy
+        options={[
+          { value: 'name-asc', label: 'Sort by name (A-Z)' },
+          { value: 'name-dsc', label: 'Sort by name (Z-A)' },
+          { value: 'regularPrice-asc', label: 'Sort by price (lowest)' },
+          { value: 'regularPrice-dsc', label: 'Sort by price (highest)' },
+          { value: 'maxCapacity-asc', label: 'Sort by capacity (lowest)' },
+          { value: 'maxCapacity-dsc', label: 'Sort by capacity (highest)' },
         ]}
       />
     </TableOperations>
